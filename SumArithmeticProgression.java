f14336b90d982c31354a1a6f7f95498719c0cce0
oleksaT <110099624+oleksaT@users.noreply.github.com>
2022-07-27 13:51:43 +0300

    Update SumArithmeticProgression.java

--- SumArithmeticProgression.java
+++ SumArithmeticProgression.java
@@ -6,14 +6,14 @@ public class SumArithmeticProgression {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of test-cases: ");
-        int count = scanner.nextInt(); // количество сумм, которые надо вывести
+        int count = scanner.nextInt();
 
         double sum = 0;
         System.out.println("Enter A D N:");
         for (int i = 0; i < count; i++) {
-            double a = scanner.nextInt(); // первое число
-            int d = scanner.nextInt(); // шаг
-            int n = scanner.nextInt(); // количество итераций
+            double a = scanner.nextInt(); // first number
+            int d = scanner.nextInt(); // step
+            int n = scanner.nextInt(); // count of iteration
 
             sum = ((2 * a + d * (n - 1)) / 2) * n;
             System.out.println(sum);
